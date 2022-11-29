7aa72d3342979c62e2d44c1b644ea77c0d749ec7
Professor047 <119353694+Professor047@users.noreply.github.com>
2022-11-29 20:13:35 +0530

    Removed Duplicate Comment

--- EvenorOddNumber.c
+++ EvenorOddNumber.c
@@ -7,7 +7,7 @@ int main()
     scanf("%d", &n);
     printf("The Number is %d\n", n);
     //This will check if the number is odd or even
-    if(n%2==0){   //This will check if the number is odd or even
+    if(n%2==0){   
         printf("%d is a Even Number\n");
     }
     else{
